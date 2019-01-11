e23a3bd96df8c1b44845a5df9378c359d596863d
Rehaan <frankfill2007@gmail.com>
2019-01-11 01:16:43 +0000

    implement webpack

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); //  -> ADDED IN THIS STEP
+// Constant with our paths
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'app'),
+  JS: path.resolve(__dirname, 'app'),
+};
+// Webpack configuration
+module.exports = {
+  entry: path.join(paths.JS, 'index.jsx'),
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js',
+  },
+  // Tell webpack to use html plugin
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, '..', 'index.html'),
+    }),
+    new ExtractTextPlugin('style.bundle.css'), // CSS will be extracted to this bundle file -> ADDED IN THIS STEP
+  ],
+  // Loaders configuration
+  // We are telling webpack to use "babel-loader" for .js and .jsx files
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+      // CSS loader for CSS files
+      // Files will get handled by css loader and then passed to the extract text plugin
+      // which will write it to the file we defined above
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      // File loader for image assets -> ADDED IN THIS STEP
+      // We'll add only image extensions, but you can things like svgs, fonts and videos
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
