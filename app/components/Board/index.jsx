60161d93a96982117053121aef76aad8ccc084f6
Rehaan <mohammad.rehaan@hotmail.com>
2019-01-11 01:25:46 -0500

    add more stuff

--- app/components/Board/index.jsx
+++ app/components/Board/index.jsx
@@ -7,7 +7,8 @@ class Board extends React.Component {
 	constructor(props) {
     super(props);
     this.state = {
-      board: []
+      board: [],
+      player: 1
     }
 	}
 	
@@ -19,6 +20,57 @@ class Board extends React.Component {
         board: Puzzle.getBoard()
       }
     });
+  }
+
+  checkWinner = (row, col) => {
+
+    var _this = this;
+
+    function checkDirection(direction) {
+      let total = 0;
+      let i = row + direction.i;
+      let j = col + direction.j;
+      while (
+        i >= 0 &&
+        i < 6 &&
+        j >= 0 &&
+        j < 7 &&
+        _this.state.board[i][j] === _this.state.player
+      ) {
+        total++;
+        i += direction.i;
+        j += direction.j;
+      }
+      return total;
+    }
+
+    function checkWin(dirA, dirB) {
+      const total = 1 + checkDirection(dirA) + checkDirection(dirB);
+      if (total >= 4) {
+        console.log(`${_this.state.player} wins!`)
+        return _this.state.player;
+      } else {
+        return null;
+      }
+    }
+
+    function checkVerticals() {
+      return checkWin({i: -1, j: 0}, {i: 1, j:0});
+    }
+
+    function checkHorizontals() {
+      return checkWin({i: 0, j: -1}, {i: 0, j: 1});
+    }
+
+    function checkBRtoTL() {
+      return checkWin({i: 1, j: -1}, {i: 1, j: 1});
+    }
+
+    function checkBLtoTR() {
+      return checkWin({i: 1, j: 1}, {i: -1, j: -1});
+    }
+
+    return checkVerticals() || checkHorizontals() || checkBRtoTL() || checkBLtoTR();
   }
 
   getNextCell = (id) => {
@@ -35,16 +87,26 @@ class Board extends React.Component {
   makeMove = (event) => {
     var id = event.target.id;
     var cell = this.getNextCell(id);
-    const { board } = this.state;
+    const { board, player } = this.state;
     var _board = board;
+    var _player = player;
     if(cell) {
-      _board[cell[0]][cell[1]] = 1;
+      var row = parseInt(cell[0]);
+      var col = parseInt(cell[1]);
+      _board[row][col] = _player;
+      if (_player === 1) {
+        _player = 2;
+      } else {
+        _player = 1;
+      }
+      this.checkWinner(row, col);
       this.setState(() => {
         return {
-          board: _board
+          board: _board,
+          player: _player
         }
-      })
-      console.log(this.state.board);
+      });
+      console.table(this.state.board);
     } else {
       console.log("Full, can't make any more moves!");
     }
@@ -54,9 +116,9 @@ class Board extends React.Component {
     var id = event.target.id;
     var cell = this.getNextCell(id);
     if(cell) {
-      console.log(cell);
+      // console.log(cell);
     } else {
-      console.log("Full, can't make any more moves!");
+      // console.log("Full, can't make any more moves!");
     }
   }
   
